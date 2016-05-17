b930512bf4fc019a9e293701f50d21d0abf50caa
alex <xalexpintax@gmail.com>
2016-05-18 02:17:42 +0300

    test package

---
+++ src/test/java/container/WinterTest.java
@@ -0,0 +1,38 @@
+package container;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Winter.
+ */
+public class WinterTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public WinterTest(String testName)
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( WinterTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
