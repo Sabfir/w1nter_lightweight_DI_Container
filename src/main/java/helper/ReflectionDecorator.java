94f56b53a7f0977fefbaf8dc43963e788e5eb265
alex <xalexpintax@gmail.com>
2016-05-19 01:26:34 +0300

    added winter methods
    refactored structure and code

--- src/main/java/helper/ReflectionDecorator.java
+++ src/main/java/helper/ReflectionDecorator.java
@@ -5,58 +5,72 @@ import container.annotation.Denied;
 import container.annotation.SnowFlake;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 
 public final class ReflectionDecorator {
+    private static List<ClassProperty> listAnnotatedClasses = new ArrayList<>();
 
-    public static List<ClassProperty> getAnnotatedClasses(String packageName) {
-        final String CLASS_SUFFIX = ".class";
-        List<ClassProperty> classes = new ArrayList<>();
-        String packageNameFormatted = "/" + packageName.replace(".", "/");
-        URL packageLocation = Thread.currentThread().getContextClassLoader().getResource(packageNameFormatted);
-        if (packageLocation == null) {
-//            LOG.warn("Could not retrieve URL resource: " + packageNameSlashed);
-            return classes;
-        }
-
-        String directoryString = packageLocation.getFile();
-        if (directoryString == null) {
-//            LOG.warn("Could not find directory for URL resource: " + packageNameSlashed);
-            return classes;
-        }
-
-        File directory = new File(directoryString);
-        if (directory.exists()) {
-            String[] files = directory.list();
-            for (String fileName : files) {
-                if (fileName.endsWith(CLASS_SUFFIX)) {
-                    fileName = fileName.substring(0, fileName.length() - 6);
-                    try {
-                        Class scannedClass = Class.forName(packageName + "." + fileName);
-                        SnowFlake snowFlake = (SnowFlake)scannedClass.getAnnotation(SnowFlake.class);
-                        if (snowFlake != null) {
-                            classes.add(getClassProperty(scannedClass, snowFlake.value()));
-                        } else {
-                            //TODO loging not found annotated class
-                        }
-                    } catch (ClassNotFoundException e) {
-//                        LOG.warn(packageName + "." + fileName + " does not appear to be a valid class.", e);
-                    }
-                }
+    public static List<ClassProperty> getAnnotatedClasses(String packageName, Class annotationType) {
+        for (Class clazz : getClasses(packageName)) {
+            Annotation annotation = clazz.getAnnotation(annotationType);
+            if (annotation != null) {
+                listAnnotatedClasses.add(getClassProperty(clazz, annotationType));
             }
-        } else {
-//            LOG.warn(packageName + " does not appear to exist as a valid package on the file system.");
+        }
+        return listAnnotatedClasses;
+    }
+    public static List<Class> getClasses(String packageName){
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+        Enumeration resources = null;
+        try {
+            resources = classLoader.getResources(path);
+        } catch (IOException e) {
+            //TODO e.printStackTrace(); can't find package
+        }
+        List<File> dirs = new ArrayList();
+        while (resources.hasMoreElements()) {
+            URL resource = (URL)resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        List<Class> classes = new ArrayList();
+        for (File directory : dirs) {
+            try {
+                classes.addAll(findClasses(directory, packageName));
+            } catch (ClassNotFoundException e) {
+                //TODO e.printStackTrace();
+            }
+        }
+        return classes;
+    }
+    public static List findClasses(File directory, String packageName) throws ClassNotFoundException {
+        List<Class> classes = new ArrayList();
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                assert !file.getName().contains(".");
+                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+            } else if (file.getName().endsWith(".class")) {
+                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+            }
         }
         return classes;
     }
 
-    public static ClassProperty getClassProperty(Class scannedClass, String beanName) {
+    public static ClassProperty getClassProperty(Class scannedClass, Class annotationType) {
         ClassProperty classProperty = new ClassProperty(scannedClass);
         classProperty.setIsCopied(scannedClass.isAnnotationPresent(Copied.class));
         classProperty.setIsDenied(scannedClass.isAnnotationPresent(Denied.class));
-        classProperty.setBeanName(beanName);
+        Annotation annotation = scannedClass.getAnnotation(annotationType);
+        classProperty.setBeanName(((SnowFlake)annotation).value());
         return classProperty;
     }
 
